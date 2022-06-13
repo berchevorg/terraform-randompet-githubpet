721b6c9ba0bf52d51a7431a6900c8204201ced3a
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-06-13 12:05:36 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+random_pet name { 
+ length    = "39"
+ separator = "-"
+}
+
+
+resource "null_resource" "helloWorld" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name.id}"
+  }
+}
